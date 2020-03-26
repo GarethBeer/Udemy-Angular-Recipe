afe659a210912940368f9bba4691fc31e8a5a739
GarethBeer <gareth.beer1989@outlook.com>
2020-03-26 08:49:23 +0000

    Components added with basic functionailty

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Spaghetti Bolognaise",
+      "A Lovely home cooked Italian Classic with Tomatoes, Garlic, Mince and Spaghetti",
+      "https://www.kitchensanctuary.com/wp-content/uploads/2019/09/Spaghetti-Bolognese-square-FS-0204.jpg"
+    ),
+    new Recipe(
+      "Spaghetti MeatBalls",
+      "A Lovely home cooked Italian Classic with Tomatoes, Garlic, Mince and Spaghetti",
+      "https://www.kitchensanctuary.com/wp-content/uploads/2019/09/Spaghetti-Bolognese-square-FS-0204.jpg"
+    )
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}

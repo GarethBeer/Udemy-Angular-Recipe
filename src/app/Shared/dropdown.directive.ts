c373c5e46d561c62d17ec17fd3a48954713cafa6
GarethBeer <gareth.beer1989@outlook.com>
2020-04-04 20:43:40 +0100

    adding directive for dropdown

---
+++ src/app/Shared/dropdown.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+    selector: '[appDropDown]'
+})
+
+export class DropDownDirective {
+  @HostBinding('class.open')  isOpen = false;
+
+@HostListener('click') toggleopen() {
+
+    this.isOpen = !this.isOpen;
+}
+
+}
